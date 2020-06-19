77380db355914b8b2cadf04185720f934a55c52b
ychuanjian <kangch1994@gmail.com>
2020-06-19 15:30:10 +0800

    使用了绝对位置数据，机器人步态还是不自然，改为固定支撑脚的方式计算机器人其余部位的位置

--- data_pre.py
+++ data_pre.py
@@ -2,6 +2,7 @@ import xlrd
 
 f = open('./Gait_Winter.csv', 'w', encoding='utf-8', newline='')
 data = xlrd.open_workbook('./Gait_Winter.xlsx')
+distance = data.sheet_by_index(0)
 table = data.sheet_by_index(4)
 # f.write('ankle_pos,ankle_vel,knee_pos,knee_vel,hip_pos,hip_vel')
 # f.write('\n')
@@ -14,6 +15,8 @@ for row in range(min_row, max_row):
     f.write(','+str(table.cell(row, 8).value))
     f.write(','+str(table.cell(row, 11).value))
     f.write(','+str(table.cell(row, 12).value))
+    f.write(','+str(distance.cell(row,2).value-distance.cell(row-1,2).value))
+    f.write(','+str(distance.cell(row,3).value))
     f.write('\n')
 row=max_row
 f.write(str(table.cell(row, 2).value))
@@ -22,4 +25,6 @@ f.write(','+str(table.cell(row, 7).value))
 f.write(','+str(table.cell(row, 8).value))
 f.write(','+str(table.cell(row, 11).value))
 f.write(','+str(table.cell(row, 12).value))
+f.write(','+str(distance.cell(row,2).value-distance.cell(row-1,2).value))
+f.write(','+str(distance.cell(row,3).value))
 f.close()
