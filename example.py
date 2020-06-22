e86b81bba727e1b9203b188962169b5359f2e548
ychuanjian <kangch1994@gmail.com>
2020-06-22 19:58:06 +0800

    步态自然，但是支撑脚有漂移现象

--- example.py
+++ example.py
@@ -5,6 +5,6 @@ from spinup.utils.test_policy import load_policy_and_env, run_policy
 
 env = gym.make("HumanoidBulletEnv-v0", render=True)
 env.reset()
-for _ in range(1000):
+for _ in range(2000):
     env.step(env.action_space.sample())
-    time.sleep(.1)
+    time.sleep(.01)
