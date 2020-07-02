edd3324d249056e3e3e2a87ce978d4af347b98f2
ychuanjian <kangch1994@gmail.com>
2020-07-02 08:54:52 +0800

    两足七连杆，10个可控关节

--- pybullet/gym_env.py
+++ pybullet/gym_env.py
@@ -10,14 +10,14 @@ if __name__ == '__main__':
     parser.add_argument('--num_runs', type=int, default=3)
     args = parser.parse_args()
 
-    eg = ExperimentGrid(name=time.strftime("%Y-%m-%d_",time.localtime())+'ppo'+'_centaur-human')
+    eg = ExperimentGrid(name=time.strftime("%d-%m-%Y_",time.localtime())+'ppo'+'_legs')
     eg.add('env_name', 'HumanoidBulletEnv-v0', '', False)
     eg.add('seed', [3*i for i in range(1, 1+args.num_runs)])
     eg.add('epochs', 3000)
     # eg.add('clip_ratio', 0.3)
     # eg.add('target_kl', 100)
     # eg.add('steps_per_epoch', 4000)
-    eg.add('ac_kwargs:hidden_sizes', [[64,32]], 'hid')
+    eg.add('ac_kwargs:hidden_sizes', [[32,16]], 'hid')
     # eg.add('ac_kwargs:activation', [torch.nn.ReLU], '')
     eg.run(ppo_pytorch, num_cpu=args.cpu)
 """ 
